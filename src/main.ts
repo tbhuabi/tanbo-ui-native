04e8f34c24f0b56db0b3ba98b07281b8f66dae23
tanbo <464328895@qq.com>
2017-08-14 10:25:00 +0800

    组件分模块

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { enableProdMode } from '@angular/core';
 import { AppModule } from './views/app.module';
 
 import './assets/scss/index.scss';
-import './assets/fonts/angular-ui/style.css';
 
 if (process.env.ENV === 'production') {
     enableProdMode();
