654588f86b51f9e88ff7676796b92f8f59d74032
tanbo <464328895@qq.com>
2017-08-29 17:35:37 +0800

    弹窗样式修复

--- src/modules/components-module/components/scroll/scroll.component.ts
+++ src/modules/components-module/components/scroll/scroll.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Component({
     selector: 'ui-scroll',
     templateUrl: './scroll.component.html'
 })
 export class ScrollComponent {
+
+    @Output()
+    refresh = new EventEmitter<number>();
+
+    @HostListener('scroll', ['$event'])
+    scroll(event: any) {
+        this.refresh.emit(event.srcElement.scrollTop);
+    }
 }
--- src/views/root-page/root-page.component.ts
+++ src/views/root-page/root-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from '../../modules/index';
+import { AlertController, ConfirmController, NavController } from '../../modules/index';
 
 import { Page1Component } from '../page1/page1.component';
 
@@ -8,14 +8,21 @@ import { Page1Component } from '../page1/page1.component';
     styleUrls: ['./root-page.component.scss']
 })
 export class RootPageComponent {
-    constructor(private navController: NavController) {
+    title: string | number = '首页';
+
+    constructor(private navController: NavController,
+                private alertController: AlertController,
+                private confirmController: ConfirmController) {
     }
 
     goToPage1() {
         this.navController.push(Page1Component);
     }
 
-    test(a: any) {
-        console.log(a);
+    test() {
+        this.alertController.show({
+            title: 'test',
+            content: 'content'
+        });
     }
 }
