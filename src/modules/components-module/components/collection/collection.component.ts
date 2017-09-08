c01f5279e65456e6f49e24a37ef1f7d657cae691
tanbo <464328895@qq.com>
2017-09-08 12:01:14 +0800

    完成collection组件

--- src/modules/components-module/components/collection/collection.component.ts
+++ src/modules/components-module/components/collection/collection.component.ts
@@ -1,9 +1,179 @@
-import { Component } from '@angular/core';
+import {
+    AfterContentInit,
+    Component,
+    ContentChildren,
+    ElementRef,
+    HostBinding,
+    Input,
+    QueryList,
+    Renderer2
+} from '@angular/core';
+import { CollectionItemComponent } from '../collection-item/collection-item.component';
 
 @Component({
     selector: 'ui-collection',
     templateUrl: './collection.component.html'
 })
-export class CollectionComponent {
+export class CollectionComponent implements AfterContentInit {
+    @Input()
+    @HostBinding('class.vertical')
+    vertical: boolean = false;
+    @ContentChildren(CollectionItemComponent)
+    items: QueryList<CollectionItemComponent>;
 
+    @HostBinding('style.width')
+    get width() {
+        return this.vertical ? 'auto' : this.childrenLength * 100 + '%';
+    }
+
+    @HostBinding('style.height')
+    get height() {
+        return this.vertical ? this.childrenLength * 100 + '%' : 'auto';
+    }
+
+    childrenLength: number = 0;
+
+    private x: number = 0;
+    private y: number = 0;
+
+    constructor(private renderer: Renderer2,
+                private elementRef: ElementRef) {
+    }
+
+    ngAfterContentInit() {
+        this.childrenLength = this.items.toArray().length;
+        if (this.vertical) {
+            this.bindingVerticalDragEvent();
+        } else {
+            this.bindingHorizontalDragEvent();
+        }
+    }
+
+    bindingHorizontalDragEvent() {
+        let element = this.elementRef.nativeElement;
+        this.renderer.listen(element, 'touchstart', (event: any) => {
+            const point = event.touches[0];
+            const startX = point.pageX;
+            const startY = point.pageY;
+            this.renderer.setStyle(element, 'transition-duration', '0s');
+            const oldX = this.x;
+
+            let unTouchMoveFn: () => void;
+            let unTouchEndFn: () => void;
+            let unTouchCancelFn: () => void;
+
+            const itemWidth = element.offsetWidth / this.childrenLength;
+            const minDistance = itemWidth - element.offsetWidth;
+
+            const unbindFn = function () {
+                unTouchMoveFn();
+                unTouchEndFn();
+                unTouchCancelFn();
+
+                const targetIndex = Math.ceil(this.x / itemWidth);
+                const offsetX = Math.abs(this.x % itemWidth);
+
+                let translateX: number;
+                if (offsetX < (itemWidth / 2)) {
+                    translateX = targetIndex * itemWidth;
+                } else {
+                    translateX = targetIndex * itemWidth - itemWidth;
+                }
+
+                this.x = translateX;
+                this.renderer.setStyle(element, 'transition-duration', '');
+                this.renderer.setStyle(element, 'transform', `translateX(${translateX}px)`);
+
+            }.bind(this);
+
+            unTouchMoveFn = this.renderer.listen('document', 'touchmove', (ev: any) => {
+                const point = ev.touches[0];
+
+                if (Math.abs(point.pageX - startX) < Math.abs(point.pageY - startY)) {
+                    unbindFn();
+                    return;
+                }
+
+                let distance = oldX + point.pageX - startX;
+                if (distance > 0) {
+                    distance = 0;
+                }
+
+                if (distance < minDistance) {
+                    distance = minDistance;
+                }
+                this.x = distance;
+
+                this.renderer.setStyle(element, 'transform', `translateX(${distance}px)`);
+                ev.preventDefault();
+                return false;
+            });
+            unTouchEndFn = this.renderer.listen('document', 'touchend', unbindFn);
+            unTouchCancelFn = this.renderer.listen('document', 'touchcance', unbindFn);
+        });
+    }
+
+    bindingVerticalDragEvent() {
+        let element = this.elementRef.nativeElement;
+        this.renderer.listen(element, 'touchstart', (event: any) => {
+            const point = event.touches[0];
+            const startX = point.pageX;
+            const startY = point.pageY;
+            this.renderer.setStyle(element, 'transition-duration', '0s');
+
+            const oldY = this.y;
+
+            let unTouchMoveFn: () => void;
+            let unTouchEndFn: () => void;
+            let unTouchCancelFn: () => void;
+
+            const itemHeight = element.offsetHeight / this.childrenLength;
+            const minDistance = itemHeight - element.offsetHeight;
+
+            const unbindFn = function () {
+                unTouchMoveFn();
+                unTouchEndFn();
+                unTouchCancelFn();
+
+                const targetIndex = Math.ceil(this.y / itemHeight);
+                const offsetY = Math.abs(this.y % itemHeight);
+
+                let translateY: number;
+                if (offsetY < (itemHeight / 2)) {
+                    translateY = targetIndex * itemHeight;
+                } else {
+                    translateY = targetIndex * itemHeight - itemHeight;
+                }
+
+                this.y = translateY;
+                this.renderer.setStyle(element, 'transition-duration', '');
+                this.renderer.setStyle(element, 'transform', `translateY(${translateY}px)`);
+
+            }.bind(this);
+
+            unTouchMoveFn = this.renderer.listen('document', 'touchmove', (ev: any) => {
+
+                const point = ev.touches[0];
+
+                if (Math.abs(point.pageX - startX) > Math.abs(point.pageY - startY)) {
+                    unbindFn();
+                    return;
+                }
+                let distance = oldY + point.pageY - startY;
+                if (distance > 0) {
+                    distance = 0;
+                }
+
+                if (distance < minDistance) {
+                    distance = minDistance;
+                }
+                this.y = distance;
+                this.renderer.setStyle(element, 'transform', `translateY(${distance}px)`);
+                ev.preventDefault();
+                return false;
+            });
+            unTouchEndFn = this.renderer.listen('document', 'touchend', unbindFn);
+            unTouchCancelFn = this.renderer.listen('document', 'touchcance', unbindFn);
+        });
+    }
 }
--- src/views/root-page/root-page.component.ts
+++ src/views/root-page/root-page.component.ts
@@ -1,48 +1,23 @@
-import { Component } from '@angular/core';
-import { AlertController, ConfirmController, NavController } from '../../modules/index';
-
-import { Page1Component } from '../page1/page1.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     templateUrl: './root-page.component.html',
     styleUrls: ['./root-page.component.scss']
 })
-export class RootPageComponent {
-    isLoading: boolean = false;
-    progress: number = 0;
-    title: string | number = '首页';
+export class RootPageComponent implements OnInit {
+    items: Array<any> = [];
 
-    constructor(private navController: NavController,
-                private alertController: AlertController,
-                private confirmController: ConfirmController) {
-    }
+    ngOnInit() {
 
-    goToPage1() {
-        this.navController.push(Page1Component);
-    }
+        let getNumber = function () {
+            return Math.floor(Math.random() * 255);
+        };
 
-    test() {
-        this.alertController.show({
-            title: 'test',
-            content: 'content'
-        });
-    }
+        for (let i = 0; i < 3; i++) {
 
-    rolling(event: number) {
-        this.isLoading = false;
-        this.progress = event;
-    }
-
-    refresh(fn: any) {
-        console.log(4444);
-        this.isLoading = true;
-        setTimeout(() => {
-            this.isLoading = false;
-            fn();
-        }, 4000);
-    }
-
-    infinite() {
-        console.log('infinite');
+            this.items.push({
+                background: `rgb(${getNumber()},${getNumber()},${getNumber()})`
+            })
+        }
     }
 }
