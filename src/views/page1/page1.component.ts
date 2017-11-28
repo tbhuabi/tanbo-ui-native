67b0174a5c73d7153ffc4911782836e8e6afea10
tanbo <464328895@qq.com>
2017-11-28 10:07:50 +0800

    更改检查策略，优化动画性能，标记 View.Sleep 脱离检测树

--- src/views/page1/page1.component.ts
+++ src/views/page1/page1.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     templateUrl: './page1.component.html'
 })
-export class Page1Component {
+export class Page1Component implements OnInit {
     progress: number = 0;
+
+    index: number = 0;
+
+    ngOnInit() {
+        setInterval(() => {
+            this.index++;
+        }, 1000);
+    }
 
     dragging(progress: number) {
         this.progress = progress;
