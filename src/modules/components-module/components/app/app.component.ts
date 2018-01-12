c9f35f1fbad5dd256d33f0abe52450f6a1c61110
tanbo <464328895@qq.com>
2018-01-12 10:47:44 +0800

    优化样式，兼容ipx

--- src/modules/components-module/components/app/app.component.ts
+++ src/modules/components-module/components/app/app.component.ts
@@ -3,6 +3,7 @@ import { PRIMARY_OUTLET } from '@angular/router';
 import { DOCUMENT, Location } from '@angular/common';
 import { Subscription } from 'rxjs';
 
+import { BrowserENV, UI_BROWSER_ENV } from '../../config';
 import { ListActivatedService } from '../list-item/list-activated.service';
 import { RouteCacheController } from '../router/route-cache-controller';
 import { AppController, getENV } from './app-controller';
@@ -20,12 +21,34 @@ export class AppComponent implements OnInit, OnDestroy {
     @HostBinding('attr.version')
     version = process.env.version;
 
+    // 根据不同环境，需要把页面头部增高相对尺寸，以显示时间，电池电量等信息，这里通过样式来控制
+    @HostBinding('class.iphone')
+    get isIphone() {
+        return this.env === BrowserENV.iphone;
+    }
+
+    @HostBinding('class.iphone-x')
+    get isIphoneX() {
+        return this.env === BrowserENV.iphoneX;
+    }
+
+    @HostBinding('class.android')
+    get isAndroid() {
+        return this.env === BrowserENV.android;
+    }
+
+    @HostBinding('class.default')
+    get isDefault() {
+        return this.env === BrowserENV.default;
+    }
+
     private htmlElement: HTMLElement;
     private defaultDocWidth: number = 320;
 
-    private sub: Subscription;
+    private subs: Array<Subscription> = [];
 
     constructor(@Inject(DOCUMENT) private document: Document,
+                @Inject(UI_BROWSER_ENV) private env: BrowserENV,
                 private routeCacheController: RouteCacheController,
                 private location: Location,
                 private appController: AppController,
@@ -33,16 +56,19 @@ export class AppComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
-        this.sub = this.location.subscribe(() => {
+        this.subs.push(this.appController.onResize$.subscribe((env: BrowserENV) => {
+            this.env = env;
+        }));
+        this.subs.push(this.location.subscribe(() => {
             this.routeCacheController.isCacheCurrentView(false);
-        }) as Subscription;
+        }) as Subscription);
 
         this.htmlElement = this.document.querySelector('html');
         this.resize();
     }
 
     ngOnDestroy() {
-        this.sub.unsubscribe();
+        this.subs.forEach(item => item.unsubscribe());
     }
 
     @HostListener('window:resize')
--- src/modules/components-module/components/page/page.component.ts
+++ src/modules/components-module/components/page/page.component.ts
@@ -1,49 +1,8 @@
-import { Component, HostBinding, OnInit, OnDestroy, Inject } from '@angular/core';
-import { Subscription } from 'rxjs';
-
-import { BrowserENV, UI_BROWSER_ENV } from '../../config';
-import { AppController } from '../app/app-controller';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'ui-page',
     templateUrl: './page.component.html'
 })
-export class PageComponent implements OnDestroy, OnInit {
-
-    // 根据不同环境，需要把页面头部增高相对尺寸，以显示时间，电池电量等信息，这里通过样式来控制
-    @HostBinding('class.iphone')
-    get isIphone() {
-        return this.env === BrowserENV.iphone;
-    }
-
-    @HostBinding('class.iphone-x')
-    get isIphoneX() {
-        return this.env === BrowserENV.iphoneX;
-    }
-
-    @HostBinding('class.android')
-    get isAndroid() {
-        return this.env === BrowserENV.android;
-    }
-
-    @HostBinding('class.default')
-    get isDefault() {
-        return this.env === BrowserENV.default;
-    }
-
-    private sub: Subscription;
-
-    constructor(@Inject(UI_BROWSER_ENV) private env: BrowserENV,
-                private appController: AppController) {
-    }
-
-    ngOnInit() {
-        this.sub = this.appController.onResize$.subscribe((env: BrowserENV) => {
-            this.env = env;
-        });
-    }
-
-    ngOnDestroy() {
-        this.sub.unsubscribe();
-    }
+export class PageComponent {
 }
