e5c5f6d2f685be864ae9e7a133ea5a6a8cc72dd9
tanbo <464328895@qq.com>
2018-06-14 09:25:12 +0800

    删除无效代码

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,9 @@ const gulpSourceMap = require('gulp-sourcemaps');
 
 gulp.task('copy', function () {
     gulp.src('./projects/tanbo/ui-native/src/assets/**/*').pipe(gulp.dest('./dist/tanbo/ui-native/assets/'));
-    // gulp.src('./projects/tanbo/ui-native/src/modules/**/*').pipe(gulp.dest('./dist/tanbo/ui-native/modules/'));
 });
 gulp.task('scss',  function () {
-    return gulp.src(['./node_modules/normalize.css/normalize.css', './projects/tanbo/ui-native/src/assets/scss/index.scss', './projects/tanbo/ui-native/src/assets/fonts/style.css'])
+    return gulp.src(['./projects/tanbo/ui-native/src/assets/scss/index.scss', './projects/tanbo/ui-native/src/assets/fonts/style.css'])
         .pipe(gulpSourceMap.init())
         .pipe(gulpSass())
         .pipe(gulpAutoPrefix())
