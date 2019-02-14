9dd87bb3a2d3bbaae153256ef859e202f0d30e1c
tanbo <464328895@qq.com>
2019-02-14 11:04:51 +0800

    timer 清理

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component, TemplateRef, ViewChild, OnInit, ComponentRef } from '@angular/core';
 
-import { ModalController, DialogController, AlertController } from '../../tanbo/ui-native/public_api';
+import {
+  ModalController,
+  DialogController,
+  AlertController,
+  ContentLoadingController
+} from '../../tanbo/ui-native/public_api';
 import { SelectComponent } from '../../tanbo/ui-native/lib/forms/select/select.component';
 
 @Component({
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   show(n: any) {
     document.title = n.scale;
+  }
+
+  constructor(private contentLoadingController: ContentLoadingController) {
+  }
+
+  ngOnInit() {
+    this.contentLoadingController.show()
   }
 }
